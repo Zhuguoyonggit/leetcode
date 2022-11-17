c4f8514e879bf84349a6eac9f166c9df18e0a4d5
zhu <ZGY16655827571@outlook.com>
2022-11-17 17:50:50 +0800

    测试

---
+++ src/leetcode/editor/cn/OneFGaJU.java
@@ -0,0 +1,100 @@
+  //给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != 
+//k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请 
+//
+// 你返回所有和为 0 且不重复的三元组。 
+//
+// 注意：答案中不可以包含重复的三元组。 
+//
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-1,0,1,2,-1,-4]
+//输出：[[-1,-1,2],[-1,0,1]]
+//解释：
+//nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+//nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+//nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+//不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+//注意，输出的顺序和三元组的顺序并不重要。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,1,1]
+//输出：[]
+//解释：唯一可能的三元组和不为 0 。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [0,0,0]
+//输出：[[0,0,0]]
+//解释：唯一可能的三元组和为 0 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 3 <= nums.length <= 3000 
+// -10⁵ <= nums[i] <= 10⁵ 
+// 
+//
+// 
+// 注意：本题与主站 15 题相同：leetcode.cn/problems/3sum/ 
+//
+// Related Topics 数组 双指针 排序 👍 89 👎 0
+
+  
+  package leetcode.editor.cn;
+  import java.util.ArrayList;
+  import java.util.Arrays;
+  import java.util.List;
+
+  public class OneFGaJU{
+      public static void main(String[] args) {
+           Solution solution = new OneFGaJU().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // 本题要去重，显然从结果中去重是不理智的，通过排序进行去从就方便了很多
+        Arrays.sort(nums);
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            // 对第一个数进行  去重,同时注意溢出问题
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                while (j > i + 1 && j < nums.length && nums[j] == nums[j - 1]) {
+                    j++;
+                }
+                // j 有可能突破k的值
+                if (j >= k) break;
+                int sum = nums[i] + nums[j] + nums[k];
+                // 这时候会发现排序真的挺好
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    // 注意这里要移动指针
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
