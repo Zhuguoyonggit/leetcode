b7d73376978155e43b0813ae77ef5cb8fad09460
zhu <ZGY16655827571@outlook.com>
2023-05-23 11:07:21 +0800

    Changes

--- src/leetcode/editor/cn/IsSubsequence.java
+++ src/leetcode/editor/cn/IsSubsequence.java
@@ -67,7 +67,7 @@
       //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        return 0;
+        return true;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
